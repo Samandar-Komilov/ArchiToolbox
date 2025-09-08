5397940025663400710485a51cc9f42023893044
Samandar-Komilov <komilov.samandar07@gmail.com>
2025-09-08 21:12:51 +0500

    add: two pointers opposite ends, one ends

---
+++ implementations/TwoPointers/tp1_3.py
@@ -0,0 +1,21 @@
+"""
+Pair Sum Exists
+"""
+
+def pair_sum_exists(arr: list[int], target: int) -> bool:
+    left, right = 0, len(arr) - 1
+
+    while (left < right):
+        curr_sum = arr[left] + arr[right]
+        if (curr_sum == target):
+            return True
+        elif (curr_sum > target):
+            right -= 1
+        elif (curr_sum < target):
+            left += 1
+        
+    return False
+
+
+print(pair_sum_exists([1,2,4,7,11,15], 16))
+print(pair_sum_exists([1,2,4,7,11,15], 23))
---
+++ implementations/TwoPointers/tp1_5.py
@@ -0,0 +1,22 @@
+"""
+Container with most water
+"""
+
+def container_with_most_water(arr: list[int]):
+    left, right = 0, len(arr) - 1
+    maxvol = 0
+
+    while (left < right):
+        currvol = (right - left) * min(arr[left], arr[right])
+        maxvol = max(maxvol, currvol)
+        print(currvol, maxvol, left, right)
+
+        if (arr[left] < arr[right]):
+            left += 1
+        else:
+            right -= 1
+        
+    return maxvol
+
+
+print(container_with_most_water([1,8,6,2,5,4,8,3,4]))
